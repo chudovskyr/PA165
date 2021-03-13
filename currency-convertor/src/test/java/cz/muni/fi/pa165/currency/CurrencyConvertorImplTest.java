8524316bdb0db757cc70223b2ba89ab5c072cfd9
Radoslav Chudovsky <469069@mail.muni.cz>
2021-03-13 15:49:52 +0100

    Implement testConvert

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,14 +1,40 @@
 package cz.muni.fi.pa165.currency;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class CurrencyConvertorImplTest {
 
+    private Currency EUR = Currency.getInstance("EUR");
+    private Currency CZK = Currency.getInstance("CZK");
+
+    @Mock
+    private ExchangeRateTable exchangeRateTable;
+
+    private CurrencyConvertor currencyConvertor;
+
+    @Before
+    public void init() {
+        currencyConvertor = new CurrencyConvertorImpl(exchangeRateTable);
+    }
+
     @Test
-    public void testConvert() {
+    public void testConvert() throws ExternalServiceFailureException {
         // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+        when(exchangeRateTable.getExchangeRate(EUR, CZK)).thenReturn(new BigDecimal("1"));
+
+        assertEquals(new BigDecimal("1"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1")));
+        assertEquals(new BigDecimal("1.06"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1.055")));
+        assertEquals(new BigDecimal("1.02"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1.025")));
+        assertEquals(new BigDecimal("1.02"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1.016")));
+        assertEquals(new BigDecimal("1.01"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1.014")));
     }
 
     @Test
