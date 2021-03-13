16454c40c25f9a64a7c5752dcb8b4dc1387e607c
Radoslav Chudovsky <469069@mail.muni.cz>
2021-03-13 16:22:43 +0100

    Fix testConvert test

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -4,8 +4,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.internal.matchers.Null;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 import java.util.Currency;
@@ -13,6 +14,7 @@ import java.util.Currency;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
 
     private final Currency EUR = Currency.getInstance("EUR");
@@ -35,7 +37,7 @@ public class CurrencyConvertorImplTest {
     public void testConvert() throws ExternalServiceFailureException {
         when(exchangeRateTable.getExchangeRate(EUR, CZK)).thenReturn(new BigDecimal("1"));
 
-        assertEquals(new BigDecimal("1"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1")));
+        assertEquals(new BigDecimal("1.00"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1")));
         assertEquals(new BigDecimal("1.06"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1.055")));
         assertEquals(new BigDecimal("1.02"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1.025")));
         assertEquals(new BigDecimal("1.02"), currencyConvertor.convert(EUR, CZK, new BigDecimal("1.016")));
@@ -73,7 +75,7 @@ public class CurrencyConvertorImplTest {
         when(exchangeRateTable.getExchangeRate(EUR, CZK))
                 .thenThrow(ExternalServiceFailureException.class);
 
-        thrown.expect(ExternalServiceFailureException.class);
+        thrown.expect(UnknownExchangeRateException.class);
         currencyConvertor.convert(EUR, CZK, new BigDecimal("1"));
     }
 
