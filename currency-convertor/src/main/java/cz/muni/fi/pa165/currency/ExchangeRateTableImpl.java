6bc87ce380f8db6d58d228f2e51da3c2eb46221f
Radoslav Chudovsky <469069@mail.muni.cz>
2021-03-19 16:05:53 +0100

    Add ExchangeRateTableImpl

---
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/ExchangeRateTableImpl.java
@@ -0,0 +1,27 @@
+package cz.muni.fi.pa165.currency;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class ExchangeRateTableImpl implements ExchangeRateTable {
+
+    private static final Currency CZK = Currency.getInstance("CZK");
+    private static final Currency EUR = Currency.getInstance("EUR");
+
+    @Override
+    public BigDecimal getExchangeRate(Currency sourceCurrency, Currency targetCurrency) throws ExternalServiceFailureException {
+        if (sourceCurrency == null) {
+            throw new IllegalArgumentException("Source currency is null.");
+        }
+
+        if (targetCurrency == null) {
+            throw new IllegalArgumentException("Target currency is null.");
+        }
+
+        if (sourceCurrency == EUR && targetCurrency == CZK) {
+            return new BigDecimal("27.00");
+        }
+
+        return null;
+    }
+}
