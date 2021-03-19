e43b27b806ffc21b7df1ca4a1f78b5d9d1ef8acb
Radoslav Chudovsky <469069@mail.muni.cz>
2021-03-19 20:40:09 +0100

    Add MainAnnotations

--- currency-convertor/src/main/java/cz/muni/fi/pa165/MainAnnotations.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/MainAnnotations.java
@@ -1,13 +1,15 @@
 package cz.muni.fi.pa165;
 
 import cz.muni.fi.pa165.currency.CurrencyConvertor;
+import cz.muni.fi.pa165.currency.CurrencyConvertorImpl;
+import cz.muni.fi.pa165.currency.ExchangeRateTableImpl;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.math.BigDecimal;
 import java.util.Currency;
 
-public class MainXml {
+public class MainAnnotations {
 
     private static final Currency CZK = Currency.getInstance("CZK");
     private static final Currency EUR = Currency.getInstance("EUR");
@@ -15,7 +17,7 @@ public class MainXml {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext
-                = new ClassPathXmlApplicationContext("applicationContext.xml");
+                = new AnnotationConfigApplicationContext(CurrencyConvertorImpl.class, ExchangeRateTableImpl.class);
 
         CurrencyConvertor currencyConvertor = applicationContext.getBean(CurrencyConvertor.class);
 
